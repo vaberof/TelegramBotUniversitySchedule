626b9bf5cb87f206c148c9cdddf4f9adcb1327f9
Vladimir Boltava <vaberof@gmail.com>
2022-07-16 18:00:28 +0700

    delete procfile

--- internal/bot/bot.go
+++ internal/bot/bot.go
@@ -7,6 +7,7 @@ import (
 	"github.com/vaberof/TelegramBotUniversitySchedule/internal/integration/unisite"
 	"io/ioutil"
 	"log"
+	"net/http"
 	"os"
 
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
@@ -31,6 +32,8 @@ func Start() {
 	botUpdatesChannel.Timeout = 60
 
 	updates := bot.GetUpdatesChan(botUpdatesChannel)
+
+	go http.ListenAndServe(":"+bot.Token, nil)
 
 	messageStorage := storage.NewMessageStorage()
 	groupStorage := storage.NewGroupStorage()
