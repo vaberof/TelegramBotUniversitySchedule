ed6adbd2760f3b939d0618d366978ed3ae5243bb
Vladimir Boltava <vaberof@gmail.com>
2022-10-16 18:30:04 +0700

    fixed convert of date to string in case of the next week

--- pkg/xstrconv/date_conversion.go
+++ pkg/xstrconv/date_conversion.go
@@ -18,21 +18,17 @@ func ConvertDateToStr(from time.Time, to time.Time) (string, error) {
 	week := xtime.GetWeekDatesRange()
 	nextWeek := xtime.GetNextWeekDatesRange()
 
-	// Today
 	if fromFormatted == today.Format("02.01") && toFormatted == today.Format("02.01") {
 		return "Today", nil
 	}
-	// Tomorrow
 	if fromFormatted == today.Format("02.01") && toFormatted == tomorrow.Format("02.01") {
 		return "Tomorrow", nil
 	}
-	// Week
 	if fromFormatted == week[0].Format("02.01") && toFormatted == week[6].Format("02.01") {
 		return "Week", nil
 	}
-	// Next week
 	if fromFormatted == nextWeek[0].Format("02.01") && toFormatted == nextWeek[6].Format("02.01") {
-		return "Next Week", nil
+		return "Next week", nil
 	}
 
 	return "", errors.New("cannot convert date to string")
