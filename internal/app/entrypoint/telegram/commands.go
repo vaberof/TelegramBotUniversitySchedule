e1814e5cc7baccfc69f0e89850b0f8356996e7e9
Vladimir Boltava <vaberof@gmail.com>
2022-10-30 13:38:21 +0700

    updated the start command message

--- internal/app/entrypoint/telegram/commands.go
+++ internal/app/entrypoint/telegram/commands.go
@@ -3,7 +3,7 @@ package telegram
 import tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 
 const startCommandMessageToTelegram string = "Как пользоваться ботом:\n" +
-	"1. Введите номер группы (БИ-11.1/БИ-11.2 и т.д.)\n" +
+	"1. Введите номер группы (БИ-11.1/МГк-21.1 и т.д.)\n" +
 	"2. Выберите день, на который хотите получить расписание\n"
 
 const unknownCommandMessageToTelegram string = "Неизвестная команда"
